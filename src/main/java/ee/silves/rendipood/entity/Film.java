a9751a3520226cc2553ab9243f4a236a55bad4f9
siimilves <112859403+Silvesb@users.noreply.github.com>
2025-10-26 14:39:28 +0200

    initial commit

---
+++ src/main/java/ee/silves/rendipood/entity/Film.java
@@ -0,0 +1,24 @@
+package ee.silves.rendipood.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Film {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String type;
+
+    private boolean rented;
+}
